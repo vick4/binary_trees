582845e5d928454890d2077b591be894021a554a
vick4 <victorisaacblw@gmail.com>
2023-11-01 16:03:49 +0000

    done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,20 +11,20 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-        new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
 
-        /* Set value for each set */
-        new_node->parent = parent;
-        new_node->n = value;
-        new_node->left = NULL;
-        new_node->right = NULL;
+	/* Set value for each set */
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-        return (new_node);
+	return (new_node);
 }
