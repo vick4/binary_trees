304bf7fe6c81fed4e404738339e83468f88a1507
vick4 <victorisaacblw@gmail.com>
2023-11-01 15:23:22 +0000

    done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,35 +4,38 @@
 
 /**
  * binary_tree_insert_left -  inserts a node as the left-child of another node
+ *
  * @parent: pointer to the parent node to insert the left-child in
  * @value: value to put in the new node
+ *
  * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-        if (parent == NULL)
-                return (NULL);
+	if (parent == NULL)
+	return (NULL);
 
-        /* Allocating memory to the new node */
-        new_node = malloc(sizeof(binary_tree_t));
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
+	/* memory to the new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
 
-        /* Set value for each set */
-        new_node->parent = parent;
-        new_node->n = value;
-        new_node->left = NULL;
-        new_node->right = NULL;
+	/* give a value for each set */
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-        if (parent->left != NULL)
-        {
-                new_node->left = parent->left;
-                parent->left->parent = new_node;
-                parent->left = new_node;
-        }
-        return (new_node);
+	if (parent->left != NULL)
+	{
+	new_node->left = parent->left;
+	parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
 }
