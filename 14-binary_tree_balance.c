36484b6a61806914b419eaabb1c37acfbe7a8919
vick4 <victorisaacblw@gmail.com>
2023-11-02 22:27:44 +0000

    done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/* prototype declarations */
 size_t find_tree_height_by_nodes(binary_tree_t *tree);
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
- *
- * @tree: pointer to the root node of the tree to measure the balance factor
- *
- * Return: balance factor of the tree, or 0 if tree is NULL
+ * @tree: pointer to the node of the tree to measure the balance factor
+ * Return: balance factor of the tree, 
+ * or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	/* declare variables to calculate the heights */
 	size_t left_tree_height = 0;
 	size_t right_tree_height = 0;
 
@@ -21,17 +20,18 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
+	/* calculate height of left and right subtrees */
 	left_tree_height = find_tree_height_by_nodes(tree->left);
 	right_tree_height = find_tree_height_by_nodes(tree->right);
 
+	/* balance factor = left height - right height */
 	return (left_tree_height - right_tree_height);
 }
 
 /**
  * find_tree_height_by_nodes - finds the height of a binary tree by counting
  * the number of nodes
- *
- * @tree: pointer to the root node of the tree to measure the height
+ * @tree: pointer to the node of the tree to measure the height
  *
  * Return: height of the tree, or 0 if tree is NULL
  */
@@ -46,7 +46,7 @@ size_t find_tree_height_by_nodes(binary_tree_t *tree)
 		return (0);
 	}
 
-	/* counting height by no of nodes not edges */
+	/* count the height by no of nodes not edges */
 	padding_height = 1;
 
 	left_height = find_tree_height_by_nodes(tree->left);
