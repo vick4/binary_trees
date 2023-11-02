73c280bcf6c576dccc40ee085608bb3f2c793d67
vick4 <victorisaacblw@gmail.com>
2023-11-02 04:45:15 +0000

    done

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -20,8 +20,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
+		leaves = binary_tree_leaves(tree->left);
+		leaves = binary_tree_leaves(tree->right);
 
 	return (leaves);
 }
