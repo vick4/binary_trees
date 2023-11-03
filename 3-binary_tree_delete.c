740bc0ef0c782588cde842d328f9bf3f9cc3b705
vick4 <victorisaacblw@gmail.com>
2023-11-03 01:49:20 +0000

    done

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,13 +5,12 @@
  * binary_tree_delete - delete the entire binary tree
  *
  * @tree: pointer to the root node of the tree to delete
-
+ *
  * Return: nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+	if (tree == NULL) {
 		return (NULL);
 	}
 
